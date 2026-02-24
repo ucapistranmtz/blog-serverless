d2cb6660015b211a89c6435cba4d94c20fd5c56b
Ulises <ucapistranmtz@gmail.com>
2026-02-24 15:50:03 -0600

    fixing Mocks

--- app/moks/posts.ts
+++ app/moks/posts.ts
@@ -10,6 +10,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "02HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -20,6 +22,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "03HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -30,6 +34,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "04HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -40,6 +46,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "05HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -50,6 +58,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "06HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -60,6 +70,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "07HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -70,6 +82,8 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
   {
     id: "08HRAQ3P5K8J7X9Q4W2N1B4M5A",
@@ -80,5 +94,7 @@ export const MOCK_POSTS: PostCard[] = [
     slug: "dynamodb-single-table-design",
     imageUrl: "/board.png", // Matches your uploaded file
     tags: ["AWS", "DynamoDB", "Serverless"],
+    author: "ulises",
+    authorId: "148834d8-10d1-708c-255b-c67f3ae34cd1",
   },
 ];
